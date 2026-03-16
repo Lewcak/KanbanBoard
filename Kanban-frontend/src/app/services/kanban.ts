41796fd0077de2bfc919af5cf102b50ff2b55961
Lewcak <karstenluca3@gmail.com>
2026-03-16 14:49:11 +0200

    Created Service methods for Board

--- Kanban-frontend/src/app/services/kanban.ts
+++ Kanban-frontend/src/app/services/kanban.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Board } from '../models/board.models';
+import { Board , Column , Task , MoveTaskRequest } from '../models/board.models';
 
 @Injectable({
   providedIn: 'root',
@@ -10,17 +10,40 @@ export class Kanban {
   private http = inject(HttpClient);
   private apiUrl = 'https://localhost:7107/api';
 
+
+  // Board Service Methods  
+
+  // Get All Boards
   getBoards() {
-  return this.http.get<Board[]>(`${this.apiUrl}/boards`)
+  return this.http.get<Board[]>(`${this.apiUrl}/Boards`)
   }
 
+  // Create a new Board
   postBoard(board: Board) {
-    return this.http.post<Board>(`${this.apiUrl}/boards`, board)
+    return this.http.post<Board>(`${this.apiUrl}/Boards`, board)
   }
 
-  getBoardId() {
-    return this.http.get<Board>(`${this.apiUrl}/boards/{id}`)
+  // Get a board by ID
+  getBoard(id: number) {
+    return this.http.get<Board>(`${this.apiUrl}/Boards/${id}`)
   }
+
+  // Edit a Board
+  putBoard(board: Board, id: number) {
+    return this.http.put<Board>(`${this.apiUrl}/Boards/${id}`, board)
+  }
+
+  // Delete a board
+  deleteBoard(id: number) {
+    return this.http.delete<Board>(`${this.apiUrl}/Boards/${id}`)
+  }
+
+
+  // Column Service Methods
+
+
+
+
 
 }
 
